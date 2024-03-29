83be4b84837c541e98351cc71d5887ba16a481f1
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-03-29 16:28:52 +0400

    Replace custom `HTTPClientResult` enum with a nested typealias over the standard `Swift.Result`

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -89,12 +89,12 @@ final class URLSessionHTTPClientTests: XCTestCase {
     return sut
   }
   
-  private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) -> HTTPClientResult {
+  private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) -> HTTPClient.Result {
     let sut = makeSUT(file: file, line: line)
     URLProtocolStub.stub(data: data, response: response, error: error)
     let exp = expectation(description: "Wait for completion")
 
-    var receivedResult: HTTPClientResult!
+    var receivedResult: HTTPClient.Result!
     sut.get(from: anyURL()) { result in
       receivedResult = result
       exp.fulfill()
@@ -107,7 +107,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
   private func resultValuesFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) -> (data: Data, response: HTTPURLResponse)? {
     let result = resultFor(data: data, response: response, error: error)
     switch result {
-    case let .success(data, response):
+    case let .success((data, response)):
       return (data, response)
     default:
       XCTFail("Expected success, got \(result) instead", file: file, line: line)
