2262c711cbbeca91b1dc936e846b4993e0e9a322
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-06 13:20:26 +0400

    Extract UIButton test helper to a separate file

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -367,16 +367,6 @@ extension FeedImageCell {
   }
 }
 
-private extension UIButton {
-  func simulateTap() {
-    allTargets.forEach { target in
-      actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-        (target as NSObject).perform(Selector($0))
-      }
-    }
-  }
-}
-
 private extension FeedViewController {
   func simulateUserInitiatedFeedReload() {
     refreshControl?.simulatePullToRefresh()
---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,19 @@
+//
+//  UIButton+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Afsal on 06/04/2024.
+//
+
+import UIKit
+
+extension UIButton {
+  func simulateTap() {
+    allTargets.forEach { target in
+      actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+        (target as NSObject).perform(Selector($0))
+      }
+    }
+  }
+}
+
