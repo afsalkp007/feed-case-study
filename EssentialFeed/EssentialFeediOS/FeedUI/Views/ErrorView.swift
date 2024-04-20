02d4cbb2984029df955030444230f66ea87cefdd
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-20 16:46:40 +0400

    Move error view to separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Afsal on 20/04/2024.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+  public var message: String?
+}
