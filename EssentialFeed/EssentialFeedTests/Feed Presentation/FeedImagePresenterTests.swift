e22d9ae0c334d62150b7ad9a63fa61bd1593ce3d
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-22 13:27:57 +0400

    create sut create to a factory method

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -16,16 +16,22 @@ final class FeedImagePresenter {
 class FeedImagePresenterTests: XCTestCase {
   
   func test_init_doesNotSendMessageToView() {
-    let view = ViewSpy()
-    
-    _ = FeedImagePresenter(view: view)
+    let (_, view) = makeSUT()
     
     XCTAssertTrue(view.messages.isEmpty)
   }
   
   // MARK: - Helpers
   
-  private struct ViewSpy {
+  private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+    let view = ViewSpy()
+    let sut = FeedImagePresenter(view: view)
+    trackForMemoryLeaks(view, file: file, line: line)
+    trackForMemoryLeaks(sut, file: file, line: line)
+    return (sut, view)
+  }
+  
+  private class ViewSpy {
     let messages = [Any]()
   }
 }
