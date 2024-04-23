ddc5b52f5df60b014975cdeaa8c4d6a5c750ad17
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-23 11:19:21 +0400

    Remove default URLSessionHTTPClient initializer parameter (dependen…
    …cy) as clients should be mindful about which `URLSession` instance to use. For example, clients may decide to use the `.shared` instance, but when testing we can inject a `URLSession` with an ephemeral configuration registered with the `URLProtocolStub` class. This way, we don't need to start/stop intercepting URL requests globally.

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -10,14 +10,9 @@ import EssentialFeed
 
 final class URLSessionHTTPClientTests: XCTestCase {
   
-  override func setUp() {
-    super.setUp()
-    URLProtocolStub.startInterceptingRequests()
-  }
-  
   override func tearDown() {
     super.tearDown()
-    URLProtocolStub.stopInterceptingRequests()
+    URLProtocolStub.removeStub()
   }
   
   func test_getFromURL_performGETRequestWithURL() {
@@ -90,7 +85,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
   // MARK: - Helpers
   
   private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
-    let sut = URLSessionHTTPClient()
+    let configuration = URLSessionConfiguration.ephemeral
+    configuration.protocolClasses = [URLProtocolStub.self]
+    let session = URLSession(configuration: configuration)
+    
+    let sut = URLSessionHTTPClient(session: session)
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
   }
@@ -166,15 +165,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
       stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
     }
     
-    static func startInterceptingRequests() {
-      URLProtocol.registerClass(URLProtocolStub.self)
-    }
-
-    static func stopInterceptingRequests() {
-      URLProtocol.unregisterClass(URLProtocolStub.self)
+    static func removeStub() {
       stub = nil
     }
-
+    
     override class func canInit(with request: URLRequest) -> Bool {
       return true
     }
