cbb23736fb3f4aa5a2bad2b4ea5d311276435d2f
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-24 16:06:12 +0400

    Move CoreDataFeedStore implementation of the FeedImageDataStore p…
    …rotocol to production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataFeedStore+FeedImageDataStore.swift
+//  EssentialFeed
+//
+//  Created by Afsal on 24/04/2024.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+  public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+    
+  }
+  
+  public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+    completion(.success(.none))
+  }
+}
