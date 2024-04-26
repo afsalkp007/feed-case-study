d938772a75a632c0786666807c692e8c4b0a22b1
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-26 15:14:58 +0400

    Remove unnecessary return statements

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -11,9 +11,9 @@ extension CoreDataFeedStore: FeedImageDataStore {
   public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
     perform { context in
       completion(Result {
-        guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
-        image.data = data
-        try? context.save()
+        let image = try ManagedFeedImage.first(with: url, in: context)
+        image?.data = data
+        try context.save()
       })
     }
   }
@@ -21,7 +21,7 @@ extension CoreDataFeedStore: FeedImageDataStore {
   public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
     perform { context in
       completion(Result {
-        return try ManagedFeedImage.first(with: url, in: context)?.data
+        try ManagedFeedImage.first(with: url, in: context)?.data
       })
     }
   }
