ace0a36e2decf96bcfccc31854cf0e7d29bdf864
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-26 16:39:25 +0400

    LocalFeedLoader.validateCache fails on deletion error of a expired …
    …cache
    Load NSManagedObjectModel instance lazily and cache it to prevent m…
    …ultiple `NSEntityDescriptions` claiming the same `NSManagedObject` model subclasses (This problem just generates warnings but could lead to undefined behavior in the future).

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
@@ -9,11 +9,6 @@ import Foundation
 import CoreData
 
 extension NSPersistentContainer {
-  enum LoadingError: Swift.Error {
-    case modelNotFound
-    case failedToLoadPersistentStores(Swift.Error)
-  }
-  
   static func load(name: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
     let description = NSPersistentStoreDescription(url: url)
     let container = NSPersistentContainer(name: name, managedObjectModel: model)
@@ -21,7 +16,7 @@ extension NSPersistentContainer {
     
     var loadError: Swift.Error?
     container.loadPersistentStores { loadError = $1 }
-    try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+    try loadError.map { throw $0 }
     
     return container
   }
@@ -31,8 +26,6 @@ extension NSManagedObjectModel {
   static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
     return bundle
       .url(forResource: name, withExtension: "momd")
-      .flatMap { url in
-        NSManagedObjectModel(contentsOf: url)
-      }
+      .flatMap { NSManagedObjectModel(contentsOf: $0) }
   }
 }
