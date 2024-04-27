3dbd5eca4437fe8485c437691ac5f601a2d0fcd3
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-04-27 16:14:52 +0400

    Extract system under test (SUT) creation into a factory method

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -64,7 +64,7 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     wait(for: [exp], timeout: 1.0)
   }
   
-  private   func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+  private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
     addTeardownBlock { [weak instance] in
       XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
     }
