474c2a3bc901808b226b174f7a3cc1c2b720d94a
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-05-06 13:42:33 +0400

    Move Combine helpers to a new file

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -14,8 +14,14 @@ import EssentialFeediOS
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
+  private var remoteURL: URL { URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")! }
+  
   private lazy var httpClient: HTTPClient = {
     URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+  }()
+  
+  private lazy var remoteFeedLoader: RemoteFeedLoader = {
+    RemoteFeedLoader(url: remoteURL, client: httpClient)
   }()
 
   private lazy var store: FeedStore & FeedImageDataStore = {
@@ -24,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       .defaultDirectoryURL()
       .appendingPathComponent("feed-store.sqlite"))
   }()
-  
+    
   private lazy var localFeedLoader: LocalFeedLoader = {
     LocalFeedLoader(store: store, currentDate: Date.init)
   }()
@@ -56,10 +62,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
   
   private func makeRemoteFeedLaoderWithLocalFallback() -> FeedLoader.Publisher {
-    let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-    
-    let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
-
     return remoteFeedLoader
       .loadPublisher()
       .caching(to: localFeedLoader)
@@ -77,107 +79,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                   .loadImageDataPublisher(from: url)
                   .caching(to: localImageLoader, using: url)
           })
-  }
-}
-
-extension FeedLoader {
-  public typealias Publisher = AnyPublisher<[FeedImage], Error>
-  
-  public func loadPublisher() -> Publisher {
-    Deferred {
-      Future(self.load)
-    }
-    .eraseToAnyPublisher()
-  }
-}
-
-extension Publisher where Output == [FeedImage] {
-  func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> {
-    handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
-  }
-}
-
-private extension FeedCache {
-  func saveIgnoringResult(_ feed: [FeedImage]) {
-    save(feed) { _ in }
-  }
-}
-
-extension Publisher {
-  func fallback(to fallbackPublisher: @escaping () -> AnyPublisher<Output, Failure>) -> AnyPublisher<Output, Failure> {
-    self.catch { _ in fallbackPublisher() }.eraseToAnyPublisher()
-  }
-}
-
-extension Publisher {
-  func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
-    receive(on: DispatchQueue.immediateWhenOnMainScheduler).eraseToAnyPublisher()
-  }
-}
-
-extension DispatchQueue {
-  
-  static var immediateWhenOnMainScheduler: ImmediateWhenOnMainQueueScheduler {
-    ImmediateWhenOnMainQueueScheduler()
-  }
-  
-  struct ImmediateWhenOnMainQueueScheduler: Scheduler {
-    typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
-    typealias SchedulerOptions = DispatchQueue.SchedulerOptions
-
-    var now: Self.SchedulerTimeType {
-      DispatchQueue.main.now
-    }
-
-    var minimumTolerance: SchedulerTimeType.Stride {
-      DispatchQueue.main.minimumTolerance
-    }
-
-    func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
-      guard Thread.isMainThread else {
-        return DispatchQueue.main.schedule(options: options, action)
-      }
-      
-      action()
-    }
-
-    func schedule(after date: SchedulerTimeType, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) {
-      DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
-    }
-
-    func schedule(after date: SchedulerTimeType, interval: SchedulerTimeType.Stride, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) -> any Cancellable {
-      DispatchQueue.main.schedule(after: date, interval: interval, tolerance: tolerance, options: options, action)
-    }
-  }
-}
-
-public extension FeedImageDataLoader {
-  typealias Publisher = AnyPublisher<Data, Error>
-  
-  func loadImageDataPublisher(from url: URL) -> Publisher {
-    var task: FeedImageDataLoaderTask?
-    
-    return Deferred {
-      Future { completion in
-        task = self.loadImageData(from: url, completion: completion)
-      }
-    }
-    .handleEvents(receiveCancel: { task?.cancel() })
-    .eraseToAnyPublisher()
-  }
-}
-
-extension Publisher where Output == Data {
-  func caching(to cache: FeedImageDataCache, using url: URL) -> AnyPublisher<Output, Failure> {
-    handleEvents(receiveOutput: { data in
-      cache.saveIgnoringResult(data, for: url)
-    }).eraseToAnyPublisher()
-  }
-}
-
-private extension FeedImageDataCache {
-  func saveIgnoringResult(_ data: Data, for url: URL) {
-    save(data, for: url) { _ in }
   }
 }
 
