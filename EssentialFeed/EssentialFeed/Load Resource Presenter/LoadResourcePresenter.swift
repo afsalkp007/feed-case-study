d3cbfd62ee9c3138ef072c0b5ec61d47ff08c7df
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-05-08 18:33:31 +0400

    Duplicate FeedPresenter as LoadResourcePresenter

---
+++ EssentialFeed/EssentialFeed/Load Resource Presenter/LoadResourcePresenter.swift
@@ -0,0 +1,35 @@
+//
+//  LoadResourcePresenter.swift
+//  EssentialFeed
+//
+//  Created by Afsal on 22/04/2024.
+//
+
+import Foundation
+
+final public class LoadResourcePresenter {
+  private let feedView: FeedView
+  private let loadingView: FeedLoadingView
+  private let errorView: FeedErrorView
+
+  public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+    self.feedView = feedView
+    self.loadingView = loadingView
+    self.errorView = errorView
+  }
+  
+  public func didStartLoadingFeed() {
+    errorView.display(.noError)
+    loadingView.display(FeedLoadingViewModel(isLoading: true))
+  }
+  
+  public func didFinishLoadingFeed(with feed: [FeedImage]) {
+    feedView.display(FeedViewModel(feed: feed))
+    loadingView.display(FeedLoadingViewModel(isLoading: false))
+  }
+  
+  public func didFinishLoadingFeed(with error: Error) {
+    loadingView.display(FeedLoadingViewModel(isLoading: false))
+    errorView.display(.error(message: Localized.Feed.loadError))
+  }
+}
