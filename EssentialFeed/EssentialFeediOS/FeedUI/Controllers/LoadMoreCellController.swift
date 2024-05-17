308fb7b0c5b081f1b6605d859bb2ca3037a184c8
Afsal <afsalkp.mohammed@gmail.com>
2024-05-17 14:45:06 +0400

    Load more on error tap

--- EssentialFeed/EssentialFeediOS/FeedUI/Controllers/LoadMoreCellController.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Controllers/LoadMoreCellController.swift
@@ -25,9 +25,18 @@ public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableVie
   }
   
   public func tableView(_ tableView: UITableView, willDisplay: UITableViewCell, forRowAt indexPath: IndexPath) {
-      guard !cell.isLoading else { return }
+    reloadIfNeeded()
+  }
+  
+  public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    reloadIfNeeded()
+  }
+  
+  private func reloadIfNeeded() {
     
-      callback()
+    guard !cell.isLoading else { return }
+    
+    callback()
   }
 }
 
